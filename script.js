ee0ef61c8a821503fd79b5991c397ac64088e9db
Grandon Smith <grandon.smith@yahoo.com>
2020-09-18 14:52:31 -0400

    basic formatting of start page and search screen done.

---
+++ script.js
@@ -0,0 +1,116 @@
+let apiKey = "d68772bd487fd3b02862992c8d3ccb7c"
+
+function listStates() {
+    return `
+    <select name="state" id="statesearch" class="search">
+        <option value="" selected="selected">Select a State</option>
+        <option value="AL">Alabama</option>
+        <option value="AK">Alaska</option>
+        <option value="AZ">Arizona</option>
+        <option value="AR">Arkansas</option>
+        <option value="CA">California</option>
+        <option value="CO">Colorado</option>
+        <option value="CT">Connecticut</option>
+        <option value="DE">Delaware</option>
+        <option value="DC">District Of Columbia</option>
+        <option value="FL">Florida</option>
+        <option value="GA">Georgia</option>
+        <option value="HI">Hawaii</option>
+        <option value="ID">Idaho</option>
+        <option value="IL">Illinois</option>
+        <option value="IN">Indiana</option>
+        <option value="IA">Iowa</option>
+        <option value="KS">Kansas</option>
+        <option value="KY">Kentucky</option>
+        <option value="LA">Louisiana</option>
+        <option value="ME">Maine</option>
+        <option value="MD">Maryland</option>
+        <option value="MA">Massachusetts</option>
+        <option value="MI">Michigan</option>
+        <option value="MN">Minnesota</option>
+        <option value="MS">Mississippi</option>
+        <option value="MO">Missouri</option>
+        <option value="MT">Montana</option>
+        <option value="NE">Nebraska</option>
+        <option value="NV">Nevada</option>
+        <option value="NH">New Hampshire</option>
+        <option value="NJ">New Jersey</option>
+        <option value="NM">New Mexico</option>
+        <option value="NY">New York</option>
+        <option value="NC">North Carolina</option>
+        <option value="ND">North Dakota</option>
+        <option value="OH">Ohio</option>
+        <option value="OK">Oklahoma</option>
+        <option value="OR">Oregon</option>
+        <option value="PA">Pennsylvania</option>
+        <option value="RI">Rhode Island</option>
+        <option value="SC">South Carolina</option>
+        <option value="SD">South Dakota</option>
+        <option value="TN">Tennessee</option>
+        <option value="TX">Texas</option>
+        <option value="UT">Utah</option>
+        <option value="VT">Vermont</option>
+        <option value="VA">Virginia</option>
+        <option value="WA">Washington</option>
+        <option value="WV">West Virginia</option>
+        <option value="WI">Wisconsin</option>
+        <option value="WY">Wyoming</option>
+    </select>
+         `
+}
+
+function formatSearch() {
+    
+}
+
+function generateHeader() {
+    return `
+    <header>
+        <input type="button" value="Back to Search" id="back-to-search">
+        <h1>Restaurant Finder</h1>
+        <input type="button" value="Sort" id="sort">
+    </header>
+    `
+}
+
+
+function generateSearchScreen() {
+    return `
+    <div class="search-screen">
+        <form class="search-form">
+            <formfield>
+                <legend><h3>Fill in the form!</h3></legend>
+                <p>
+                    <label for="citysearch">City or Town: </label>
+                    <input type="text" placeholder="boston" id="citysearch" class="search">
+                </p>
+                <p>
+                    <label for="statesearch">State Initials: </label>
+                    ${listStates()}
+                </p>
+                <p>OR</p>
+                <p>
+                    <label for="restaurantsearch">Restaurant: </label>
+                    <input type="text" placeholder="Restaurant Name" id="restaurantsearch" class="search">
+                </p>
+                <p>
+                <input type="submit" id="submitsearch" class="submitsearch" value="Search">
+                </p>
+            </formfield>
+        </form>
+    </div>
+    `
+}
+
+function renderSearchScreen() {
+    $('main').on('click','#jsLetsGo', event => {
+        $('main').html(generateSearchScreen())
+    })
+};
+
+function runApp() {
+    renderSearchScreen()
+}
+
+
+$(runApp())
