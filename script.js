953d8b1367f79af457a28bee65cf34269c8984d7
Grandon Smith <grandon.smith@yahoo.com>
2020-09-22 13:07:47 -0400

    quick commit

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 'use strict';
 
-let apiKey = "d68772bd487fd3b02862992c8d3ccb7c"
-let baseUrl = "https://developers.zomato.com/api/v2.1"
+let zomatoApiKey = "d68772bd487fd3b02862992c8d3ccb7c";
+let zomatoUrl = "https://developers.zomato.com/api/v2.1";
+let bingMapsApiKey = "AnnFV0tkI_aBvhCKFib2wC518fghzw5ibbxd1WuV6U72bzXZf8KJrqA-wG2afdNH";
+let bingMapsUrl = "https://dev.virtualearth.net/REST/v1/Locations?key=AnnFV0tkI_aBvhCKFib2wC518fghzw5ibbxd1WuV6U72bzXZf8KJrqA-wG2afdNH&query=";
 
 function listStates() {
     return `
-    <select name="state" id="statesearch" class="search">
+    <select name="state" id="statesearch" class="search" required>
         <option value="" selected="selected">Select a State</option>
         <option value="AL">Alabama</option>
         <option value="AK">Alaska</option>
@@ -62,6 +64,84 @@ function listStates() {
          `
 }
 
+let store = {
+    city: "",
+    state: "",
+    name: ""
+
+};
+
+//----------------------GET CITY ID AND NARROWING SEARCH QUERY DOWN------------------------------
+
+function storeLongAndLat(responseJson) {
+    console.log(responseJson)
+}
+
+function makeSecondFetch(mapsQuery) {
+    // const options = {
+    //     headers: new Headers({
+    //     "user-key": bingMapsUrl
+    //     })
+    // };
+    fetch(mapsQuery)
+        .then(response => response.json())
+        .then(responseJson => storeLongAndLat(responseJson))
+        .catch(err => {
+            $('header').append(`'something went wrong!' ${err.message}`);
+        });
+};
+
+function formatMapsQuery(name) {
+    let mapsQuery = encodeURI(bingMapsUrl + name)
+    makeSecondFetch(mapsQuery)
+}
+
+
+function renderMoreCriteriaScreen() {
+    return `
+    <div class="search-screen">
+        <form class="search-form">
+            <formfield>
+                <legend><h3>What kind of food do you want?</h3></legend>
+                <p>
+                    <label for="cuisine">Cuisine: </label>
+                    <input type="text" placeholder="Italian" id="cuisine" class="search" required>
+                </p>
+                <p>
+                <input type="button" id="submitsearch" class="submitsearch" value="Search">
+                </p>
+            </formfield>
+        </form>
+    </div>
+    `
+}
+ 
+function saveGetLongAndLat() {
+    $('main').on('click', '.item', event => {
+        event.preventDefault();
+        let name = $('.item').attr('id')
+        console.log(name)
+        store.name = name;
+
+        $('main').html(renderMoreCriteriaScreen());
+        formatMapsQuery(name);
+    })
+}
+
+
+
+function clickBackToSearch() {
+
+    $('header').on('click', '#backToSearch', event => {
+        event.preventDefault()
+        console.log("Back to search ran")
+//         $('main').empty();
+        $('main').html(generateSearchScreen());
+    })
+}
+
+
+//------------------- EVENT LISTENER FOR STATE AND CITY INPUTS----------------------
 
 function displayCityMatches(responseJson, state) {
     console.log(responseJson);
@@ -69,20 +149,23 @@ function displayCityMatches(responseJson, state) {
     let array = responseJson.location_suggestions;
     for (let i = 0; i < array.length; i++) {
         if (array[i].state_code === state) {
-        divs += `<div class="item"><button>#CITY in ${array[i].state_code}</button></div>`
+        divs += `
+        <div class="item" id="${array[i].name}"><button>${array[i].name}</button></div>
+        `
         }
     }
     $('main').html(`
+    <div class="center"><h3>Choose Location:</h3></div>
         <div class="group">${divs}</div>
         `
     )
     $('header').html(generateHeader())
-}
+};
 
 function makeFirstFetch(query, state) {
     const options = {
         headers: new Headers({
-        "user-key": apiKey
+        "user-key": zomatoApiKey
         })
     };
     fetch(query, options)
@@ -91,7 +174,7 @@ function makeFirstFetch(query, state) {
         .catch(err => {
             $('header').append(`'something went wrong!' ${err.message}`);
         });
-}
+};
 
 
 function formatSearch() {
@@ -99,19 +182,19 @@ function formatSearch() {
         event.preventDefault();
         let city = $('#citysearch').val().toLowerCase();
         let state = $('#statesearch').val();
-        let query = encodeURI(baseUrl + "/cities?q=" + city)
+        let query = encodeURI(zomatoUrl + "/cities?q=" + city)
         console.log(query);
         // is there a better way to pass state to displayCityMatches()??
         makeFirstFetch(query, state);
-        
     })
-
-}
+};
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//-------------------------INITIAL SCREEN RENDERING---------------------------------
 
 function generateHeader() {
     return `
     <header>
-        <input type="button" value="Back to Search" id="back-to-search" >
+        <input type="submit" value="Back to Search" id="backToSearch" class="abc" >
         <h1>Restaurant Finder</h1>
         <input type="button" value="Sort" id="sort" class="hidden">
     </header>
@@ -127,7 +210,7 @@ function generateSearchScreen() {
                 <legend><h3>In What city do you want to eat?</h3></legend>
                 <p>
                     <label for="citysearch">City: </label>
-                    <input type="text" placeholder="boston" id="citysearch" class="search">
+                    <input type="text" placeholder="boston" id="citysearch" class="search" required>
                 </p>
                 <p>
                 ${listStates()}
@@ -143,13 +226,17 @@ function generateSearchScreen() {
 
 function renderSearchScreen() {
     $('main').on('click','#jsLetsGo', event => {
-        $('main').html(generateSearchScreen())
+        event.preventDefault();
+        console.log("renderSearchScreen ran");
+        $('main').html(generateSearchScreen());
     })
 };
 
 function runApp() {
     renderSearchScreen();
     formatSearch();
+    saveGetLongAndLat();
+    clickBackToSearch();
 }
 
 
